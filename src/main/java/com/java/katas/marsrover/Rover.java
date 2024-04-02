43b5dc9e7e6599da93fa1fb1b38c3970a289d028
Joseluis Alias <joseluis.alias@e2ycommerce.com>
2024-04-02 19:04:51 +0200

    new(infrastructure): create the first classes to implement the Kata

---
+++ src/main/java/com/java/katas/marsrover/Rover.java
@@ -0,0 +1,7 @@
+package com.java.katas.marsrover;
+
+public class Rover {
+    String execute(String commands) {
+        return "";
+    }
+}
--- src/test/java/com/java/katas/marsrover/RoverShould.java
+++ src/test/java/com/java/katas/marsrover/RoverShould.java
@@ -1,13 +1,25 @@
 package com.java.katas.marsrover;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RoverShould {
 
-	@Test
-	void contextLoads() {
+	/**
+	 * "L, 0:0:W",
+	 * "LL, 0:0:S",
+	 * "LLL, 0:0:E",
+	 * "LLLL, 0:0:N"
+	 */
+
+	@ParameterizedTest
+	@ValueSource(strings = { "1", "3", "5", "-3", "15", "Integer.MAX_VALUE" })
+	void forNowTheRoverDoesntDoAnything(String input) {
+		assertTrue(true);
 	}
 
 }
