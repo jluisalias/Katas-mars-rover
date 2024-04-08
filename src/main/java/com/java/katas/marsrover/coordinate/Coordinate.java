5e69aba5d9b41bc91bc218ec46628cf288587513
Joseluis Alias <joseluis.alias@e2ycommerce.com>
2024-04-08 19:04:57 +0200

    feat(coordinates): add coordinates functionality

---
+++ src/main/java/com/java/katas/marsrover/coordinate/Coordinate.java
@@ -0,0 +1,37 @@
+package com.java.katas.marsrover.coordinate;
+
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+
+public class Coordinate {
+    private final int x;
+    private final int y;
+
+    Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public String returnFormattedCoordinate() {
+        return new StringBuilder().append(this.x).append(":")
+                .append(this.y).toString();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return reflectionEquals(this, other);
+    }
+
+    @Override
+    public int hashCode() {
+        return reflectionHashCode(this);
+    }
+}
